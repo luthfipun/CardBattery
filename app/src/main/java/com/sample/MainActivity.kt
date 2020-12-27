529862879adf5eeb353026de33b31781d50eb946
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2020-12-27 11:01:45 +0700

    version 1.0

---
+++ app/src/main/java/com/sample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                views.setProgress(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
+}
---
+++ cardbattrey/src/main/java/luthfipun/cardbattrey/CardBattery.kt
@@ -0,0 +1,141 @@
+package luthfipun.cardbattrey
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    companion object {
+        private const val DEFAULT_RADIUS = 10
+        private const val DEFAULT_BG = Color.GRAY
+        private const val DEFAULT_PROGRESS = 0
+        private const val DEFAULT_PROGRESS_COLOR = Color.BLUE
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var bgColor = DEFAULT_BG
+    private var radius = DEFAULT_RADIUS
+    private var progress = DEFAULT_PROGRESS
+    private var progressColor = DEFAULT_PROGRESS_COLOR
+    private var loading = 0
+
+    init {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+
+        val typeArray: TypedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.CardBattery, 0 , 0)
+
+        bgColor = typeArray.getColor(R.styleable.CardBattery_cbBackground, DEFAULT_BG)
+        radius = typeArray.getInt(R.styleable.CardBattery_cbRadius, DEFAULT_RADIUS)
+        progress = typeArray.getInt(R.styleable.CardBattery_cbProgress, DEFAULT_PROGRESS)
+        progressColor = typeArray.getColor(R.styleable.CardBattery_cbProgress_color, DEFAULT_PROGRESS_COLOR)
+
+        typeArray.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        drawBackground(canvas)
+        drawProgress(canvas)
+    }
+
+    private fun drawProgress(canvas: Canvas?) {
+
+        paint.color = progressColor
+        paint.style = Paint.Style.FILL
+
+        val rectF = RectF()
+        rectF.set(
+            0f,
+            calculateProgress(),
+            width.toFloat(),
+            height.toFloat()
+        )
+
+        val corners = floatArrayOf(
+            calculateRoundTop(), calculateRoundTop(),   // Top left radius in px
+            calculateRoundTop(), calculateRoundTop(),   // Top right radius in px
+            radius.toFloat(), radius.toFloat(),     // Bottom right radius in px
+            radius.toFloat(), radius.toFloat()      // Bottom left radius in px
+        )
+
+        val path = Path()
+        path.addRoundRect(rectF, corners, Path.Direction.CW)
+        canvas?.drawPath(path, paint)
+    }
+
+    private fun calculateProgress(): Float {
+        loading = ((progress / 100.0) * height).toInt() // 0 - height
+        if (loading < radius) {
+            return ((height - loading) - radius).toFloat()
+        }
+
+        return (height - loading).toFloat()
+    }
+
+    private fun drawBackground(canvas: Canvas?) {
+
+        paint.color = bgColor
+        paint.style = Paint.Style.FILL
+
+        val rectF = RectF()
+        rectF.set(0f,
+            0f,
+            width.toFloat(),
+            height.toFloat())
+
+        canvas?.drawRoundRect(
+            rectF,
+            radius.toFloat(), radius.toFloat(), paint
+        )
+    }
+
+    fun setProgress(int: Int) {
+        if (progress != int){
+            progress = int
+            invalidate()
+        }
+    }
+
+    private fun calculateRoundTop(): Float {
+        if (loading > height - radius){
+            return ((radius - (height - loading)).toFloat())
+        }
+
+        return 0f
+    }
+
+    fun setRadius(int: Int) {
+        if (radius != int){
+            radius = int
+            invalidate()
+        }
+    }
+
+    fun setBackground(colorInt: Int){
+        if (bgColor != colorInt) {
+            bgColor = colorInt
+            invalidate()
+        }
+    }
+
+    fun setProgressColor(colorInt: Int) {
+        if (progressColor != colorInt) {
+            progressColor = colorInt
+            invalidate()
+        }
+    }
+
+    fun getProgress(): Int {
+        return progress
+    }
+
+    fun getRadius(): Int {
+        return radius
+    }
+}
