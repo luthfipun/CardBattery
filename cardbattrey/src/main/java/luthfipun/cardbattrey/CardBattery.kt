0b1d3e31233463e81563ac550f4ea4370cca4e1a
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2020-12-27 14:34:16 +0700

    Fix typo and added show progress status

--- cardbattrey/src/main/java/luthfipun/cardbattrey/CardBattery.kt
+++ cardbattrey/src/main/java/luthfipun/cardbattrey/CardBattery.kt
@@ -13,6 +13,7 @@ class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs
         private const val DEFAULT_BG = Color.GRAY
         private const val DEFAULT_PROGRESS = 0
         private const val DEFAULT_PROGRESS_COLOR = Color.BLUE
+        private const val DEFAULT_SHOW_PROGRESS = true
     }
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -21,6 +22,7 @@ class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs
     private var progress = DEFAULT_PROGRESS
     private var progressColor = DEFAULT_PROGRESS_COLOR
     private var loading = 0
+    private var showProgress = DEFAULT_SHOW_PROGRESS
 
     init {
         init(attrs)
@@ -33,7 +35,8 @@ class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs
         bgColor = typeArray.getColor(R.styleable.CardBattery_cbBackground, DEFAULT_BG)
         radius = typeArray.getInt(R.styleable.CardBattery_cbRadius, DEFAULT_RADIUS)
         progress = typeArray.getInt(R.styleable.CardBattery_cbProgress, DEFAULT_PROGRESS)
-        progressColor = typeArray.getColor(R.styleable.CardBattery_cbProgress_color, DEFAULT_PROGRESS_COLOR)
+        progressColor = typeArray.getColor(R.styleable.CardBattery_cbProgressColor, DEFAULT_PROGRESS_COLOR)
+        showProgress = typeArray.getBoolean(R.styleable.CardBattery_cbShowProgress, DEFAULT_SHOW_PROGRESS)
 
         typeArray.recycle()
     }
@@ -45,28 +48,29 @@ class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs
     }
 
     private fun drawProgress(canvas: Canvas?) {
+        if (showProgress){
+            paint.style = Paint.Style.FILL
+            paint.color = progressColor
 
-        paint.color = progressColor
-        paint.style = Paint.Style.FILL
+            val rectF = RectF()
+            rectF.set(
+                0f,
+                calculateProgress(),
+                width.toFloat(),
+                height.toFloat()
+            )
 
-        val rectF = RectF()
-        rectF.set(
-            0f,
-            calculateProgress(),
-            width.toFloat(),
-            height.toFloat()
-        )
+            val corners = floatArrayOf(
+                calculateRoundTop(), calculateRoundTop(),   // Top left radius in px
+                calculateRoundTop(), calculateRoundTop(),   // Top right radius in px
+                radius.toFloat(), radius.toFloat(),     // Bottom right radius in px
+                radius.toFloat(), radius.toFloat()      // Bottom left radius in px
+            )
 
-        val corners = floatArrayOf(
-            calculateRoundTop(), calculateRoundTop(),   // Top left radius in px
-            calculateRoundTop(), calculateRoundTop(),   // Top right radius in px
-            radius.toFloat(), radius.toFloat(),     // Bottom right radius in px
-            radius.toFloat(), radius.toFloat()      // Bottom left radius in px
-        )
-
-        val path = Path()
-        path.addRoundRect(rectF, corners, Path.Direction.CW)
-        canvas?.drawPath(path, paint)
+            val path = Path()
+            path.addRoundRect(rectF, corners, Path.Direction.CW)
+            canvas?.drawPath(path, paint)
+        }
     }
 
     private fun calculateProgress(): Float {
@@ -137,5 +141,14 @@ class CardBattery(context: Context?, attrs: AttributeSet?) : View(context, attrs
 
     fun getRadius(): Int {
         return radius
+    }
+
+    fun setShowProgress(boolean: Boolean) {
+        showProgress = boolean
+        invalidate()
+    }
+
+    fun isShowProgress(): Boolean {
+        return showProgress
     }
 }
